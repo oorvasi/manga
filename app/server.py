950e1af24600934ebb2a5fb09fa2bf3d9fc50809
oorvasi sooprayen <minie_oorvasi@hotmail.com>
2020-07-02 12:31:57 +0200

    commit

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://colab.research.google.com/drive/155twHzi7GoqRsvNQLR5cwn-ipi7wHggg?usp=sharing'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Assassination Classroom', 'One Piece', 'Seven Deadly Sins']
 path = Path(__file__).parent
 
 app = Starlette()
